9ddbad10ae39dd1379f71d8f85c0a56da7c828a8
andy78644 <andy690100391@gmail.com>
2023-04-22 12:33:56 +0800

    Handle EIP-1967 Transparent Proxy

--- pages/api/address/info.ts
+++ pages/api/address/info.ts
@@ -1,5 +1,6 @@
-import { InfuraProvider, isAddress, JsonRpcProvider } from "ethers";
+import { InfuraProvider, isAddress, JsonRpcProvider, Provider } from "ethers";
 import { NextApiRequest, NextApiResponse } from "next";
+import { ethers } from "ethers";
 
 export type AddressInfo = {
   address: string;
@@ -33,11 +34,12 @@ export default async function handler(
   const { address, chainId } = req.query;
 
   if (!address || !chainId) return res.status(400).json({ message: "Bad request" });
-
+  console.log(chainId);
   if (!isAddress(address)) {
     res.status(400).json({ message: "Invalid address format" });
     return;
   }
+  //chainId = '1';
   const provider = new InfuraProvider(
     parseInt(chainId as string),
     process.env.INFURA_API_KEY
@@ -49,17 +51,36 @@ export default async function handler(
     isContract,
   };
   if (isContract) {
-    info.contractABI = await getContractABI(address as string);
+    let proxy = await isTransparentProxy(address, provider);
+    let enteraddress = proxy === '0x00'?address:proxy;
+    info.contractABI = await getContractABI(enteraddress as string);
   }
 
   return res.status(200).json(info);
 }
 
 async function getContractABI(address: string) {
+  
   const response = await fetch(
-    `https://api.arbiscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.ARBISCAN_API_KEY}`
+    `https://api.arbiscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.ARBISCAN_API_KEY}&position=3`
   );
   const data = await response.json();
-  if (data.status !== "1") return null;
+  if (data.status !== "1") return null;   
   return data.result;
 }
+
+
+const BEACON_PROXY_STORAGE_SLOTS = {
+  implementation: 1,
+  admin: 2,
+  pendingAdmin: 3
+};
+
+
+async function isTransparentProxy(proxyAddress: string, provider: Provider): Promise<string> {
+  const code = await provider.getCode(proxyAddress);
+  const target = ethers.toBeHex(await provider.getStorage(proxyAddress, '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',));
+  //console.log(typeof(target));
+  return target;
+}
+
