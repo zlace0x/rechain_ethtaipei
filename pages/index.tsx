c7f7a7aded497f44c3de1dcffd08fc430030965d
zlace0x <zlace0x@gmail.com>
2023-04-22 12:45:33 +0800

    refactor: moved into custom store

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ChainFlux</title>
+        <title>Rechain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
